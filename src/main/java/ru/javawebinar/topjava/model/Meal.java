87bdb008bcae6fa16406fd2c1f7079e40c39e777
VitaliiKlimov <alex-ustasa@yandex.ru>
2020-10-29 23:24:32 +1000

    5 0 fix

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -1,19 +1,38 @@
 package ru.javawebinar.topjava.model;
 
-import javax.persistence.FetchType;
-import javax.persistence.ManyToOne;
+import org.hibernate.validator.constraints.Range;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
+@Entity
+@Table(name="meals",uniqueConstraints = {@UniqueConstraint(columnNames = "user_id",name = "date_time")})
 public class Meal extends AbstractBaseEntity {
+
+    public static final String ALL_SORTED = "Meal.getAll";
+    public static final String DELETE = "Meal.delete";
+    public static final String GET_BETWEEN = "Meal.getBetween";
+
+    @Column(name="date_time",nullable = false)
+    @NotNull
     private LocalDateTime dateTime;
 
+    @Column(name="description",nullable = false)
+    @NotBlank
     private String description;
 
+    @Column(name="calories",nullable = false)
+    @Range(min = 10, max = 10000)
     private int calories;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="user_id")
+    @NotNull
     private User user;
 
     public Meal() {
