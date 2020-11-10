5ed0586ec9d5f364e15a03a82ead42c9c9635ddf
VitaliiKlimov <alex-ustasa@yandex.ru>
2020-11-10 17:16:59 +1000

    6 10 cascade ddl

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -1,5 +1,7 @@
 package ru.javawebinar.topjava.model;
 
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 import org.hibernate.validator.constraints.Range;
 
 import javax.persistence.*;
